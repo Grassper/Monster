97e4094728bd622749c4dad5bf7c5e930f8dd5e4
Grassper <rishi.bharath27@gmail.com>
2020-06-16 21:18:30 +0530

    Updates

---
+++ precache-manifest.6677074fa88742fd4bcc9d6c30803919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539d4620812dfb2ea370cd4286ddd27f",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "feaffd22baae2e168aa7",
+    "url": "/Monster/static/css/main.612d4a85.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/Monster/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaffd22baae2e168aa7",
+    "url": "/Monster/static/js/main.aa0efd59.chunk.js"
+  },
+  {
+    "revision": "9287d174c171a77f8ef6",
+    "url": "/Monster/static/js/runtime-main.6dc95c68.js"
+  }
+]);
